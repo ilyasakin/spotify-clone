72e89f75bbd2dafa64ee46e2e897e17bdfd62592
İlyas <ashnwor@gmail.com>
2019-09-27 19:00:54 +0300

    general/js: createListItem() function

--- js/clientScript.js
+++ js/clientScript.js
@@ -10,4 +10,24 @@ let play = true;
                 play = true;
             }
         });
-    });
+    function createListItem(text) {
+    var musicList = document.getElementById("actualMusicList");
+    var listElement = document.createElement("li");
+    listElement.classList.add("list-group-item");
+    var write = document.createTextNode(text);
+    listElement.appendChild(write);
+    musicList.appendChild(listElement);
+    
+    }
+
+    for (i=1; i<10; i++) {
+        createListItem("Test " + i);
+    }
+
+
+    
+//  var write = document.createTextNode("Test");
+// listElement.classList.add("list-group-item");
+//listElement.appendChild(write);
+    musicList.appendChild(listElement); 
+});
