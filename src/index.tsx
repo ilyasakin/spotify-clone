13b08c9a7bcd136f288d6dfa6ed098c6f4d5be69
İlyas <ashnwor@gmail.com>
2020-06-13 01:28:22 +0300

    misc: remove serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { setConfig } from 'react-hot-loader';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 setConfig({
   ignoreSFC: true,
@@ -16,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
