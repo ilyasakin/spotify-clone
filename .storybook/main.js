5184dd66b8f10fdc89e6fc3e7216098ab784d245
İlyas <ashnwor@gmail.com>
2020-06-14 22:30:29 +0300

    misc: misc include stories from components directory

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../stories/**/*.stories.js', '../src/components/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
