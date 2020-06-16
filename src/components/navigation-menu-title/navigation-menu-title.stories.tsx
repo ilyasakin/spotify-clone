46241a5196e6d9525d528afd689915a771d781fc
İlyas <ashnwor@gmail.com>
2020-06-17 00:14:35 +0300

    general: create navbar title component

---
+++ src/components/navigation-menu-title/navigation-menu-title.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavigationMenuTitle from './navigation-menu-title';
+import './navigation-menu-title.scss';
+
+export default {
+  title: 'NavigationMenuTitle',
+};
+
+export const navigationMenuTitle = () => {
+  return (
+    <div className="story">
+      <NavigationMenuTitle />
+    </div>
+  );
+};
---
+++ src/components/navigation-menu-title/navigation-menu-title.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './navigation-menu-title.scss';
+
+const NavigationMenuTitle: React.FC = () => {
+  return <div className="menu-title">PLAYLISTS</div>;
+};
+
+export default NavigationMenuTitle;
