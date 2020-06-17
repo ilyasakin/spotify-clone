1ac9947405e8dc040234fa433e496808a3cfd6a2
İlyas <ashnwor@gmail.com>
2020-06-17 20:53:49 +0300

    general: add logo-container component

---
+++ src/components/logo-container/logo-container.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './logo-container.scss';
+import Logo from '../icons/Logo';
+
+const LogoContainer = () => {
+  return (
+    <div className="logo-container">
+      <Logo className="logo-img" />
+    </div>
+  );
+};
+
+export default LogoContainer;
