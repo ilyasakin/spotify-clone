2517a9b8646a22d3346332947a07bd1d64af1632
İlyas <ashnwor@gmail.com>
2020-06-17 15:46:49 +0300

    general: add navigation divider component

---
+++ src/components/navigation-divider/navigation-divider.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavigationDivider from './navigation-divider';
+
+export default {
+  title: 'NavigationDivider',
+};
+
+export const navigationDivider = () => {
+  return <NavigationDivider />;
+};
---
+++ src/components/navigation-divider/navigation-divider.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './navigation-divider.scss';
+
+const NavigationDivider = () => {
+  return <div className="divider" />;
+};
+
+export default NavigationDivider;
