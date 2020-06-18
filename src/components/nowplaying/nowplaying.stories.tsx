2a117684f497e47979c8d28d4bb47d0573512ef3
İlyas <ashnwor@gmail.com>
2020-06-18 19:26:58 +0300

    general: add nowplaying component

---
+++ src/components/nowplaying/nowplaying.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nowplaying from './nowplaying';
+
+export default {
+  title: 'Nowplaying',
+};
+
+export const nowplaying = () => (
+  <div style={{ position: 'relative', top: 90 }}>
+    <Nowplaying />
+  </div>
+);
---
+++ src/components/nowplaying/nowplaying.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './nowplaying.scss';
+import NowplayingLeft from '../nowplaying-left/nowplaying-left';
+import NowplayingRight from '../nowplaying-right/nowplaying-right';
+import NowplayingCenter from '../nowplaying-center/nowplaying-center';
+
+const Nowplaying = () => {
+  return (
+    <div className="nowplaying">
+      <div className="nowplaying-padding">
+        <NowplayingLeft songName="song name" artistName="artist name" />
+        <NowplayingCenter currentTime="0:00" totalTime="0:00" />
+        <NowplayingRight />
+      </div>
+    </div>
+  );
+};
+
+export default Nowplaying;
