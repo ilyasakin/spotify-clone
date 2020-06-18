2e5ff0ffc810ed77bdb993da03c395a6a5792998
İlyas <ashnwor@gmail.com>
2020-06-18 19:15:37 +0300

    general: add nowplaying right component

---
+++ src/components/nowplaying-right/nowplaying-right.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './nowplaying-right.scss';
+import { Playlist, Devices, Volume } from '../icons';
+
+const NowplayingRight = () => {
+  return (
+    <div className="nowplaying-right-container">
+      <div className="nowplaying-right-playlist">
+        <Playlist className="nowplaying-right-icon" />
+      </div>
+      <div className="nowplaying-right-devices">
+        <Devices className="nowplaying-right-icon-devices" />
+      </div>
+      <div className="nowplaying-right-volume">
+        <Volume className="nowplaying-right-icon" />
+      </div>
+      <div className="nowplaying-right-volume-bar" />
+    </div>
+  );
+};
+
+export default NowplayingRight;
