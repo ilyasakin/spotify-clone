8722a734d3a83ff458ca26aa9940f7e1aa9fd5ba
İlyas <ashnwor@gmail.com>
2020-06-18 19:31:05 +0300

    general: add nowplaying bar to app screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import './styles/App.scss';
 import Navbar from './components/navbar/navbar';
+import Nowplaying from './components/nowplaying/nowplaying';
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <Nowplaying />
+    </div>
+  );
 }
 
 export default hot(App);
