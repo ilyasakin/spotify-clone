fe9423024374e41f61bc3c0f8155d13c39b2ef31
İlyas <ashnwor@gmail.com>
2020-06-22 01:42:24 +0300

    general: add topbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { hot } from 'react-hot-loader/root';
 import './styles/App.scss';
 import Navbar from './components/navbar/navbar';
 import Nowplaying from './components/nowplaying/nowplaying';
+import Topbar from './components/topbar/topbar';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Nowplaying />
+      <Topbar />
     </div>
   );
 }
---
+++ src/components/topbar/topbar.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Topbar from './topbar';
+
+export default {
+  title: 'Topbar',
+};
+
+export const topbar = () => <Topbar />;
