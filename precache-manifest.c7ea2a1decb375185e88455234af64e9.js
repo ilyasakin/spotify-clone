37bf0f3823c837f36cc626ac2e7dd9f248866d81
İlyas <ashnwor@gmail.com>
2020-07-24 20:25:26 +0300

    Updates

---
+++ precache-manifest.c7ea2a1decb375185e88455234af64e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6354ccf17930dc023799b999b74656f2",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "f58e3b0c6379a23232af",
+    "url": "/spotify-clone/static/css/main.a87266fa.chunk.css"
+  },
+  {
+    "revision": "09dc096f618587837e65",
+    "url": "/spotify-clone/static/js/2.ac879e7e.chunk.js"
+  },
+  {
+    "revision": "6fc5f1effa5dcecce30c825b510f7b99",
+    "url": "/spotify-clone/static/js/2.ac879e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58e3b0c6379a23232af",
+    "url": "/spotify-clone/static/js/main.00c47d76.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
