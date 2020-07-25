12a9127eceaae8a8ee2df8dfa3b61b31b39d97cb
İlyas <ashnwor@gmail.com>
2020-07-25 06:53:40 +0300

    Updates

---
+++ precache-manifest.4ffe24d10d64c75191455cc385c90805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a525b44ba72b292accbab8385909a81",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "5329e7fc4ccdd97c58e4",
+    "url": "/spotify-clone/static/css/main.50fa7cf3.chunk.css"
+  },
+  {
+    "revision": "ff6ae526f395628f9d28",
+    "url": "/spotify-clone/static/js/2.9daf9279.chunk.js"
+  },
+  {
+    "revision": "51bad7197f80ba6de638761bb11ce3be",
+    "url": "/spotify-clone/static/js/2.9daf9279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5329e7fc4ccdd97c58e4",
+    "url": "/spotify-clone/static/js/main.413143ab.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-clone/precache-manifest.5de3229d450708df66d6851df7ed6eae.js"
+  "/spotify-clone/precache-manifest.4ffe24d10d64c75191455cc385c90805.js"
 );
 
 self.addEventListener('message', (event) => {
