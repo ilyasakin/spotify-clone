30535d8b3785d664ead4975c65ca43b770e2b014
İlyas <ashnwor@gmail.com>
2020-07-28 01:32:25 +0300

    misc: rewrite card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -10,21 +10,30 @@ interface Props {
 const Card: React.FC<Props> = ({ song }) => {
   const { setCurrentSong } = useContext(CurrentSong);
   return (
-    <div className="card-container">
-      <div className="card-img-container">
+    <div className="card-content">
+      <div className="card-cover-container">
         <img
-          style={{ width: '100%' }}
-          src={`${process.env.REACT_APP_BASE_URL}/${song.cover}`}
+          className="card-cover"
           alt=""
+          src={`${process.env.REACT_APP_BASE_URL}/${song.cover}`}
         />
       </div>
-      <span className="card-title">{song.name}</span>
-      <span className="card-artist">{song.artist}</span>
+      <div className="card-info-area">
+        <div className="card-title-area">
+          <span className="card-title" title={song.name}>
+            {song.name}
+          </span>
+        </div>
+        <div className="card-description-area" title={song.artist}>
+          {song.artist}
+        </div>
+      </div>
       <div
         className="card-fab"
         role="button"
         tabIndex={0}
         aria-hidden="true"
+        title="Play"
         onClick={() => setCurrentSong?.(song)}
       >
         <CardPlay className="card-fab-icon" />
