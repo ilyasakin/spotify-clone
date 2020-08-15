fbd6ad137842a3da8938657d067acf6aa839429e
İlyas <ashnwor@gmail.com>
2020-08-16 01:50:11 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-clone/precache-manifest.ea4ace03163ecd140af2af2b8e97cc0e.js"
+  "/spotify-clone/precache-manifest.ad6137ac3402806e22b3606c8cb87f47.js"
 );
 
 self.addEventListener('message', (event) => {
