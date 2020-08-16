c93fea1c98c48c97692c860e5410f094e4cb02b4
İlyas <ashnwor@gmail.com>
2020-08-16 20:37:08 +0300

    misc: fix color issue on menu

--- src/components/NavigationPlaylistMenuItem/NavigationPlaylistMenuItem.tsx
+++ src/components/NavigationPlaylistMenuItem/NavigationPlaylistMenuItem.tsx
@@ -14,7 +14,7 @@ const NavigationPlaylistMenuItem: React.FC<Props> = ({ Icon, text, gradient }) =
         <div
           className={`playlist-menu-icon-container ${gradient && 'playlist-menu-icon-gradient'}`}
         >
-          <Icon className="playlist-menu-icon" />
+          <Icon className={`playlist-menu-icon ${gradient ? 'white-color' : ''}`} />
         </div>
         <span className="playlist-menu-text">{text}</span>
       </div>
