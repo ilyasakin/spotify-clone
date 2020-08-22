ee6f96852b789dfed66a1020b323153cf34c10a9
İlyas <ashnwor@gmail.com>
2020-08-22 22:01:57 +0300

    misc: song type

--- src/context/CurrentSong.ts
+++ src/context/CurrentSong.ts
@@ -1,7 +1,14 @@
 import { createContext, Dispatch, SetStateAction } from 'react';
 
 interface Props {
-  currentSong: Record<string, never>;
+  currentSong: {
+    _id?: string;
+    id?: number;
+    name?: string;
+    artist?: string;
+    cover?: string;
+    location?: string;
+  };
   setCurrentSong: Dispatch<SetStateAction<{}>>;
 }
 
