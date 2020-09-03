011af05b17d3ea5a95b3fdaf175f1a2708b6e34f
İlyas <ashnwor@gmail.com>
2020-09-03 15:19:41 +0300

    general: some routing work on overview

--- src/screens/Overview.tsx
+++ src/screens/Overview.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import '../styles/App.scss';
 import '../styles/Overview.scss';
-import { useHistory } from 'react-router-dom';
+import { Route, Switch, Redirect, useHistory, useRouteMatch, Link } from 'react-router-dom';
 import Axios from 'axios';
 import moment from 'moment';
 import TopbarOverview from '../components/TopbarOverview/TopbarOverview';
@@ -17,6 +17,7 @@ const Overview = () => {
   document.title = 'Account Overview - Spotify';
   const { user, setUser } = useContext(User);
   const history = useHistory();
+  const match = useRouteMatch();
 
   const parseDate = (date: string | undefined): string => {
     if (date) {
@@ -25,7 +26,6 @@ const Overview = () => {
     }
     return 'Not found';
   };
-
   return (
     <div className="overview-background" style={{ height: '100%' }}>
       <TopbarOverview />
@@ -47,38 +47,63 @@ const Overview = () => {
                 borderRadius: '50%',
               }}
             />
-            <OverviewMenuItem text="Account overview" indicator />
-            <OverviewMenuItem text="Edit profile" />
+            <Link to={`${match.path}/account`} style={{ textDecoration: 'none' }}>
+              <OverviewMenuItem
+                text="Account overview"
+                indicator={history.location.pathname === '/overview/account' && true}
+              />
+            </Link>
+            <Link to={`${match.path}/edit`} style={{ textDecoration: 'none' }}>
+              <OverviewMenuItem
+                text="Edit profile"
+                indicator={history.location.pathname === '/overview/edit' && true}
+              />
+            </Link>
           </div>
           <OverviewPage>
-            <h1 className="overview-page-title">Account overview</h1>
-            <h3 className="overview-page-subtitle">Profile</h3>
-            <OverviewTable>
-              <OverviewTableItem label="Username" info={user?.name} />
-              <OverviewTableItem label="Email" info={user?.email} />
-              <OverviewTableItem label="Date of birth" info={parseDate(user?.birthDate)} />
-              <OverviewTableItem label="Country" info={user?.country} />
-            </OverviewTable>
-            <BigButton text="Edit Profile" variation="pop" className="overview-big-button" />
-            <h3 className="overview-page-subtitle">Signout everywhere</h3>
-            <p className="overview-paragraph">
-              Sign out wherever you have Spotify open, including the web, mobile, desktop or any
-              other devices.
-            </p>
-            <BigButton
-              text="Sign out everywhere"
-              variation="pop"
-              className="overview-big-button"
-              onClick={() => {
-                Axios.post(`${process.env.REACT_APP_BASE_URL}/api/users/logoutall`, null, {
-                  headers: { Authorization: `Bearer ${localStorage.getItem('__TOKEN')}` },
-                }).then(() => {
-                  localStorage.removeItem('__TOKEN');
-                  setUser?.({});
-                  history.push('/');
-                });
-              }}
-            />
+            <Switch>
+              <Route path={`${match.path}/account`}>
+                <h1 className="overview-page-title">Account overview</h1>
+                <h3 className="overview-page-subtitle">Profile</h3>
+                <OverviewTable>
+                  <OverviewTableItem label="Username" info={user?.name} />
+                  <OverviewTableItem label="Email" info={user?.email} />
+                  <OverviewTableItem label="Date of birth" info={parseDate(user?.birthDate)} />
+                  <OverviewTableItem label="Country" info={user?.country} />
+                </OverviewTable>
+                <BigButton
+                  text="Edit Profile"
+                  variation="pop"
+                  className="overview-big-button"
+                  onClick={() => history.push(`${match.path}/edit`)}
+                />
+                <h3 className="overview-page-subtitle">Signout everywhere</h3>
+                <p className="overview-paragraph">
+                  Sign out wherever you have Spotify open, including the web, mobile, desktop or any
+                  other devices.
+                </p>
+                <BigButton
+                  text="Sign out everywhere"
+                  variation="pop"
+                  className="overview-big-button"
+                  onClick={() => {
+                    Axios.post(`${process.env.REACT_APP_BASE_URL}/api/users/logoutall`, null, {
+                      headers: { Authorization: `Bearer ${localStorage.getItem('__TOKEN')}` },
+                    }).then(() => {
+                      localStorage.removeItem('__TOKEN');
+                      setUser?.({});
+                      history.push('/');
+                    });
+                  }}
+                />
+              </Route>
+              <Route path={`${match.path}/edit`}>
+                <h1 className="overview-page-title">Edit profile</h1>
+              </Route>
+              <Route path={`${match.path}/`}>
+                <Redirect to={`${match.url}/account`} />
+              </Route>
+            </Switch>
           </OverviewPage>
         </div>
       </div>
