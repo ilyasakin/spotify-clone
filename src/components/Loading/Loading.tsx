60bfbd0e1ec8f981dcb6bcc8ea672ae8e1109252
İlyas <ashnwor@gmail.com>
2020-09-04 16:24:39 +0300

    misc: add loading text

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -6,6 +6,7 @@ const Loading = () => {
   return (
     <div className="loading-container">
       <RotateSpinner color="#1db954" />
+      <h6 className="loading-text">Connecting to server</h6>
     </div>
   );
 };
