c7b57865c26b8e7c3bf1a13ef260aeedc15242dd
İlyas <ashnwor@gmail.com>
2020-09-12 18:30:51 +0300

    general: add button to player on overview menu

--- src/components/TopbarOverview/TopbarOverview.tsx
+++ src/components/TopbarOverview/TopbarOverview.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './TopbarOverview.scss';
+import { useHistory } from 'react-router-dom';
 import { Logo } from '../icons';
 
 const TopbarOverview = () => {
+  const history = useHistory();
+
   return (
     <div style={{ backgroundColor: '#000' }}>
       <div className="topbar-overview-container">
         <Logo className="topbar-overview-logo" />
+        <nav className="topbar-overview-nav" role="navigation">
+          <ul className="topbar-overview-nav-list">
+            <li>
+              <button
+                className="topbar-overview-nav-button"
+                onClick={() => history.push('/player')}
+              >
+                Player
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
