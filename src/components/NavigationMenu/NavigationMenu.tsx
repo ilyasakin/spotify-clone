89e699ee875f567aa8a8297e8b07435d20b1e11d
İlyas <ashnwor@gmail.com>
2020-09-22 01:42:44 +0300

    general: initial mobile navigation bar

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -3,9 +3,13 @@ import './NavigationMenu.scoped.scss';
 import NavigationButton from '../NavigationButton/NavigationButton';
 import { HomeActive, Search, Library } from '../icons';
 
-const NavigationMenu: React.FC = () => {
+interface Props {
+  bottom?: boolean;
+}
+
+const NavigationMenu: React.FC<Props> = ({ bottom }) => {
   return (
-    <div className="navigation-menu">
+    <div className={`navigation-menu ${bottom ? 'bottom-menu' : ''}`}>
       <NavigationButton Icon={HomeActive} text="Home" active />
       <NavigationButton Icon={Search} text="Search" />
       <NavigationButton Icon={Library} text="Library" />
--- src/components/Nowplaying/Nowplaying.tsx
+++ src/components/Nowplaying/Nowplaying.tsx
@@ -3,6 +3,7 @@ import './Nowplaying.scoped.scss';
 import NowplayingLeft from '../NowplayingLeft/NowplayingLeft';
 import NowplayingRight from '../NowplayingRight/NowplayingRight';
 import NowplayingCenter from '../NowplayingCenter/NowplayingCenter';
+import NavigationMenu from '../NavigationMenu/NavigationMenu';
 
 const Nowplaying: React.FC = () => {
   return (
@@ -10,6 +11,9 @@ const Nowplaying: React.FC = () => {
       <NowplayingLeft />
       <NowplayingCenter />
       <NowplayingRight />
+
+      {/* For mobile */}
+      <NavigationMenu bottom />
     </div>
   );
 };
