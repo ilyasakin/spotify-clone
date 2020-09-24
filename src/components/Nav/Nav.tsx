33d187b01178d3a15563d5aa192988cc156c48d1
İlyas <ashnwor@gmail.com>
2020-09-24 17:44:35 +0300

    general: bind player screens to react router

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,20 +1,35 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './Nav.scoped.scss';
+import { useHistory } from 'react-router-dom';
 import NavButton from '../NavButton/NavButton';
 import { HomeActive, Search, Library } from '../icons';
-import PlayerScreen from '../../context/PlayerScreen';
 
 interface Props {
   bottom?: boolean;
 }
 
 const Nav: React.FC<Props> = ({ bottom }) => {
-  const { setCurrentScreen } = useContext(PlayerScreen);
+  const history = useHistory();
+
   return (
     <div className={`navigation-menu ${bottom ? 'bottom-menu' : ''}`}>
-      <NavButton Icon={HomeActive} text="Home" active onClick={() => setCurrentScreen?.('Home')} />
-      <NavButton Icon={Search} text="Search" onClick={() => setCurrentScreen?.('Search')} />
-      <NavButton Icon={Library} text="Library" />
+      <NavButton
+        Icon={HomeActive}
+        text="Home"
+        onClick={() => history.push('/player/home')}
+        active={history.location.pathname === '/player/home'}
+      />
+      <NavButton
+        Icon={Search}
+        text="Search"
+        onClick={() => history.push('/player/search')}
+        active={history.location.pathname === '/player/search'}
+      />
+      <NavButton
+        Icon={Library}
+        text="Library"
+        active={history.location.pathname === '/player/location'}
+      />
     </div>
   );
 };
--- src/screens/Player.tsx
+++ src/screens/Player.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../styles/App.scss';
 import '../styles/Player.scss';
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Nowplaying from '../components/Nowplaying/Nowplaying';
 import Topbar from '../components/Topbar/Topbar';
@@ -8,14 +9,13 @@ import Main from '../components/Main/Main';
 import CurrentSong from '../context/CurrentSong';
 import Volume from '../context/Volume';
 import PlayingStatus from '../context/PlayingStatus';
-import PlayerScreen from '../context/PlayerScreen';
 import Search from '../components/Search/Search';
 
 const Player = () => {
   document.title = 'Spotify';
   const [currentSong, setCurrentSong] = useState({});
   const [playing, setPlaying] = useState(false);
-  const [currentScreen, setCurrentScreen] = useState<'Home' | 'Search' | 'Library'>('Home');
+  const match = useRouteMatch();
 
   const initialVolume = () => {
     const volume = localStorage.getItem('VOLUME');
@@ -27,26 +27,32 @@ const Player = () => {
   const [volume, setVolume] = useState(initialVolume());
 
   return (
-    <PlayerScreen.Provider value={{ currentScreen, setCurrentScreen }}>
-      <CurrentSong.Provider value={{ currentSong, setCurrentSong }}>
-        <PlayingStatus.Provider value={{ playing, setPlaying }}>
-          <Volume.Provider value={{ volume, setVolume }}>
-            <div className="main-container">
-              <div className="nav-content">
-                <Navbar />
-                <div className="topbar-main">
-                  <Topbar />
-                  {/* TODO: use React Router */}
-                  {currentScreen === 'Home' && <Main />}
-                  {currentScreen === 'Search' && <Search />}
-                </div>
+    <CurrentSong.Provider value={{ currentSong, setCurrentSong }}>
+      <PlayingStatus.Provider value={{ playing, setPlaying }}>
+        <Volume.Provider value={{ volume, setVolume }}>
+          <div className="main-container">
+            <div className="nav-content">
+              <Navbar />
+              <div className="topbar-main">
+                <Topbar />
+                <Switch>
+                  <Route path={`${match.path}/home`}>
+                    <Main />
+                  </Route>
+                  <Route path={`${match.path}/search`}>
+                    <Search />
+                  </Route>
+                  <Route path={`${match.path}/`}>
+                    <Redirect to={`${match.url}/home`} />
+                  </Route>
+                </Switch>
               </div>
-              <Nowplaying />
             </div>
-          </Volume.Provider>
-        </PlayingStatus.Provider>
-      </CurrentSong.Provider>
-    </PlayerScreen.Provider>
+            <Nowplaying />
+          </div>
+        </Volume.Provider>
+      </PlayingStatus.Provider>
+    </CurrentSong.Provider>
   );
 };
 
