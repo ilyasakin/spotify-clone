8f77075b94d18b13723134c874f5cb965492d7ba
İlyas <ashnwor@gmail.com>
2020-09-26 03:33:03 +0300

    misc: update home icon

--- src/components/icons/Home.tsx
+++ src/components/icons/Home.tsx
@@ -4,7 +4,7 @@ function SvgHome(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" {...props}>
       <path
-        d="M12 1.394L1.263 7.298v15.507h6.803V14.44h7.927v8.365h6.744V7.255L12 1.396zM12 0l12 6.34V24h-9.27v-8.365h-5.4V24H0V6.398L12 0z"
+        d="M12.013 2.852l-8.06 4.94V20.77H9.06v-7h5.95v7h5.063V7.757l-8.06-4.905zm0-1.167l9.008 5.306v14.777h-6.959v-7h-4.054v7H3.004V7.038l9.009-5.353z"
         fill="currentColor"
       />
     </svg>
--- src/components/icons/HomeActive.tsx
+++ src/components/icons/HomeActive.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 function SvgHomeActive(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" {...props}>
-      <path d="M24 24h-9.333v-8.39H9.333V24H0V6.278L12 0l12 6.235V24z" fill="currentColor" />
+      <path d="M21 21.738h-7v-7h-4v7H3V6.952l9-5.238 9 5.203v14.821z" fill="currentColor" />
     </svg>
   );
 }
