f54003447f34a69bafc49d0c86e1adb87dbcf17b
İlyas <ashnwor@gmail.com>
2020-09-28 00:44:05 +0300

    general: add play-pause to list-item & add playing animation

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import ImageFadeIn from 'react-image-fade-in';
+import CurrentSong from '../../context/CurrentSong';
+import PlayingStatus from '../../context/PlayingStatus';
 import Song from '../../types/Song';
+import { CardPause, CardPlay } from '../icons';
 import LikeButton from '../LikeButton/LikeButton';
 import './ListItem.scoped.scss';
 
@@ -9,18 +12,60 @@ interface Props {
   index: number;
 }
 
-const ListItem: React.FC<Props> = ({ song, index }) => (
-  <div className="list-item">
-    <div className="id">{index}</div>
-    <ImageFadeIn className="cover" src={`${process.env.REACT_APP_BASE_URL}/${song.cover}`} />
-    <div className="song-info">
-      <div className="name">{song.name}</div>
-      <div className="artist">{song.artist}</div>
+const ListItem: React.FC<Props> = ({ song, index }) => {
+  const [onHover, setOnHover] = useState(false);
+  const { currentSong, setCurrentSong } = useContext(CurrentSong);
+  const { playing, setPlaying } = useContext(PlayingStatus);
+
+  return (
+    <div
+      className="list-item"
+      onMouseEnter={() => setOnHover(true)}
+      onMouseLeave={() => setOnHover(false)}
+    >
+      <div className="id">
+        {!onHover ? (
+          <>
+            {currentSong === song && playing ? (
+              <div>
+                <div className="bar" />
+                <div className="bar" />
+                <div className="bar" />
+                <div className="bar" />
+              </div>
+            ) : (
+              index
+            )}
+          </>
+        ) : (
+          <>
+            {!playing || currentSong !== song ? (
+              <CardPlay
+                onClick={() => {
+                  setCurrentSong?.(song);
+                  setPlaying?.(true);
+                }}
+              />
+            ) : (
+              <CardPause
+                onClick={() => {
+                  setPlaying?.(false);
+                }}
+              />
+            )}
+          </>
+        )}
+      </div>
+      <ImageFadeIn className="cover" src={`${process.env.REACT_APP_BASE_URL}/${song.cover}`} />
+      <div className="song-info">
+        <div className="name">{song.name}</div>
+        <div className="artist">{song.artist}</div>
+      </div>
+      <div className="rest">
+        <LikeButton forSong={song} />
+      </div>
     </div>
-    <div className="rest">
-      <LikeButton forSong={song} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default ListItem;
