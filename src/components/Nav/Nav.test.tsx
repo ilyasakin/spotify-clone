04ae59f8f242547cebd32bc46d1dd60245a27aa9
İlyas <ashnwor@gmail.com>
2020-10-04 22:45:17 +0300

    misc: update snapshots

--- src/components/Nav/Nav.test.tsx
+++ src/components/Nav/Nav.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { HashRouter } from 'react-router-dom';
 import Nav from './Nav';
 
 it('runs correctly', () => {
-  const wrapper = shallow(<Nav />);
-  expect(wrapper.find('div').children()).toHaveLength(3);
+  const wrapper = shallow(
+    <HashRouter>
+      <Nav />
+    </HashRouter>,
+  );
+
+  expect(wrapper.find('.navigation-menu')).toHaveLength(3);
   expect(wrapper).toMatchSnapshot();
 });
