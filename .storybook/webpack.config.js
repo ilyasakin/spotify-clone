d86f9c826646e203690f0bcde657fcb535ec8660
İlyas Akın <ashnwor@gmail.com>
2020-12-01 18:27:20 +0300

    misc: Add sass-loader to Storybook Webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,25 @@
+const path = require('path');
+
 module.exports = ({ config, mode }) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: [require.resolve('babel-preset-react-app')],
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [require.resolve('babel-preset-react-app')],
+          },
         },
-      },
-      require.resolve('react-docgen-typescript-loader'),
-    ],
-  });
+        require.resolve('react-docgen-typescript-loader'),
+      ],
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    },
+  );
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
