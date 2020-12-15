ffded87085a3f648b0c21562a3d6733af6f024cf
İlyas Akın <ashnwor@gmail.com>
2020-12-15 23:19:30 +0300

    general: Implement searching song

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Search.scoped.scss';
+import Axios from 'axios';
+import { ImpulseSpinner } from 'react-spinners-kit';
 import SearchContext from '../../context/Search';
+import Song from '../../types/Song';
+import ListItem from '../ListItem/ListItem';
+import { ReactComponent as NoResult } from '../../assets/images/no-result.svg';
 
 const Search: React.FC = () => {
   const { search } = useContext(SearchContext);
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await Axios.get(
+        `${process.env.REACT_APP_BASE_URL}/api/music/search/${search}`,
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem('__TOKEN')}` },
+        },
+      );
+
+      setLoading(false);
+      setResult(response.data);
+    };
+
+    fetchData();
+  }, [search]);
+
+  if (loading) {
+    return (
+      <div className="search no-result">
+        <ImpulseSpinner frontColor="#1db954" />
+      </div>
+    );
+  }
+
+  if (result.length === 0) {
+    return (
+      <div className="search no-result">
+        <NoResult className="no-result-image" />
+      </div>
+    );
+  }
 
   return (
     <div className="search">
-      <h1>{search}</h1>
+      {result.map((song: Song, index) => {
+        return <ListItem song={song} index={index + 1} />;
+      })}
     </div>
   );
 };
