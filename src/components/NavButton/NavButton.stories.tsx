3765bf382e3b675f0187562ab5a0ed676ca15fc9
İlyas Akın <ashnwor@gmail.com>
2020-12-24 01:26:28 +0300

    misc: Make Navigation Button story better

--- src/components/NavButton/NavButton.stories.tsx
+++ src/components/NavButton/NavButton.stories.tsx
@@ -1,24 +1,29 @@
 import NavButton from './NavButton';
-import { Home, HomeActive, Search, SearchActive, Library, LibraryActive } from '../icons';
+import { Home, HomeActive } from '../icons';
 
 export default {
   title: 'Navigation Button',
 };
 
-export const navigationButton: React.FC = () => {
+export const inactive: React.FC = () => {
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
-      <div style={{ display: 'grid', rowGap: 10 }}>
+    <div
+      style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}
+    >
+      <div style={{ display: 'block', width: 300 }}>
         <NavButton Icon={Home} text="Home" />
+      </div>
+    </div>
+  );
+};
+
+export const active: React.FC = () => {
+  return (
+    <div
+      style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}
+    >
+      <div style={{ display: 'block', width: 300 }}>
         <NavButton Icon={HomeActive} text="Home" active />
-      </div>
-      <div style={{ display: 'grid', rowGap: 10 }}>
-        <NavButton Icon={Search} text="Search" />
-        <NavButton Icon={SearchActive} text="Search" active />
-      </div>
-      <div style={{ display: 'grid', rowGap: 10 }}>
-        <NavButton Icon={Library} text="Library" />
-        <NavButton Icon={LibraryActive} text="Library" active />
       </div>
     </div>
   );
