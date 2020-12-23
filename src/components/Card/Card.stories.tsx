462e8a2ab26c826fd3d0c01cc69c48718a881a04
İlyas Akın <ashnwor@gmail.com>
2020-12-24 01:25:11 +0300

    misc: Make card story better

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -7,20 +7,26 @@ export default {
 export const card: React.FC = () => {
   return (
     <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(164px, 1fr))',
-        columnGap: 48,
-      }}
+      style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}
     >
-      <Card
-        song={{
-          _id: 'story_id_1',
-          name: 'Title',
-          artist: 'Artist',
-          cover: 'https://via.placeholder.com/150',
-        }}
-      />
+      <div style={{ display: 'block' }}>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(164px, 1fr))',
+            columnGap: 48,
+          }}
+        >
+          <Card
+            song={{
+              _id: 'story_id_1',
+              name: 'Title',
+              artist: 'Artist',
+              cover: 'https://via.placeholder.com/150',
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
