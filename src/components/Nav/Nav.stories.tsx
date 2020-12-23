e0aa788fc0372a44e2fce1fdc3b587cadbd9b7b4
İlyas Akın <ashnwor@gmail.com>
2020-12-24 01:26:01 +0300

    misc: Make navigation story bettr

--- src/components/Nav/Nav.stories.tsx
+++ src/components/Nav/Nav.stories.tsx
@@ -4,4 +4,10 @@ export default {
   title: 'Navigation Menu',
 };
 
-export const navigationMenu: React.FC = () => <NavMenu />;
+export const navigationMenu: React.FC = () => (
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+    <div style={{ display: 'block', width: 300 }}>
+      <NavMenu />
+    </div>
+  </div>
+);
