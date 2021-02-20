09529c806e34caa95528631d15716b03f5ba0199
İlyas Akın <ashnwor@gmail.com>
2021-02-20 14:51:42 +0300

    Make main container more readable

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -12,19 +12,21 @@ interface Props {
 const Main: React.FC<Props> = ({ songs }) => {
   const { recentlyPlayed } = useContext(RecentlyPlayed);
 
-  return (
-    <div className={styles.main}>
-      {songs.length < 1 ? (
+  if (songs.length < 1) {
+    return (
+      <div className={styles.main}>
         <div className={styles['loading-indicator']}>
           <ImpulseSpinner frontColor="#1db954" />
         </div>
-      ) : (
-        <>
-          <Section title="Songs" data={songs} />
-          {recentlyPlayed && recentlyPlayed.length >= 1 && (
-            <Section title="Recently Played" data={recentlyPlayed} noSeeAll />
-          )}
-        </>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.main}>
+      <Section title="Songs" data={songs} />
+      {(recentlyPlayed as Song[]).length >= 1 && (
+        <Section title="Recently Played" data={recentlyPlayed as Song[]} noSeeAll />
       )}
     </div>
   );
